63e2fecc232c38e49e7390f617d6f6f86566b296
harunakonohira <harunakonohira613@gmail.com>
2026-07-07 18:02:19 +0900

    導入セクションの作成

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,21 @@ export default function home() {
   return (
     <div className={styles.twoColumn}>
       <Sidebar />
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <section className={styles.intro}>
+          <h2 className={styles.h2}>what’s “Konohira’s tech notes”?</h2>
+          <p className={styles.text}>
+            Things I thought I might forget, items for copy-pasting, things I
+            found difficult,
+            <br />
+            new things I learned, things I came to understand, and so on.
+            <br />
+            すみません、メモはにほんごでかきます。
+          </p>
+        </section>
+        <div className={styles.new}></div>
+        <div className={styles.category}></div>
+      </main>
     </div>
   );
 }
